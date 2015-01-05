7226a2c548f68cf96c2f154fccada26b7b7d244b
fuzzbomb7 <andrew@inmyroom.org>
2015-01-05 15:30:49 -0600

    Initial commit

---
+++ Mixtape Maker/Mixtape Maker/MainWindow.xaml.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Win32;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Mixtape_Maker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            dtaCompilation.DataContext = Compilation;
+        }
+
+        // The collection containing the playlist
+        ObservableCollection<CompilationFile> Compilation = new ObservableCollection<CompilationFile>();
+        
+        // The class that represents each file in the playlist
+        public class CompilationFile : INotifyPropertyChanged
+        {
+            public CompilationFile(string setPath, string setFile, string setExt)
+            {
+                this.Path = setPath;
+                this.FileName = setFile;
+                this.Extension = setExt;
+            }
+
+            // Properties
+            private string _fileName;
+            private string _oldFileName;
+
+            public string FileName 
+            {
+                get { return _fileName; }
+                set
+                {
+                    _oldFileName = _fileName;
+                    _fileName = value;
+                    OnPropertyChanged("FileName");
+                    RenameFile();
+                }
+            }
+
+            private string Path { get;  set; }
+            private string Extension { get;  set; }
+
+            // Methods
+            private void RenameFile()
+            {
+                if (_fileName == null || _oldFileName == null || _fileName == string.Empty || _oldFileName == string.Empty) return;
+                
+                string currentPath = System.IO.Path.Combine(Path, _oldFileName + Extension);
+                if (File.Exists(currentPath))
+                {
+                    string newPath = System.IO.Path.Combine(Path, _fileName + Extension);
+                    try
+                    {
+                        File.Move(currentPath, newPath);
+                    }
+                    catch(Exception ex)
+                    {
+                        string msg = "Could not rename file." + "\n" + "Current file: " + currentPath + "\n" + "New file: " + newPath + "\n" + "Exception: " + ex.Message;
+                        MessageBox.Show(msg, "File rename error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+
+            public override string ToString()
+            {
+                string fullPath = System.IO.Path.Combine(Path, FileName) + Extension;
+                return (fullPath);
+            }
+
+            // Events
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected void OnPropertyChanged(string name)
+            {
+                PropertyChangedEventHandler handler = PropertyChanged;
+                if (handler != null)
+                {
+                    handler(this, new PropertyChangedEventArgs(name));
+                }
+            }
+        }
+
+        private void btnAddFiles_Click(object sender, RoutedEventArgs e)
+        {
+            string[] files = SelectFiles("MP3 Files|*.mp3");
+            if (files != null) AddFiles(files);
+        }
+
+        private void btnAddPlaylist_Click(object sender, RoutedEventArgs e)
+        {
+            string[] files = SelectFiles("M3U Playlist|*.m3u");
+            if (files != null) AddPlaylist(files);
+        }
+
+        public string[] SelectFiles(string fileFilter)
+        {
+            OpenFileDialog select = new OpenFileDialog();
+            select.Filter = fileFilter;
+            select.Multiselect = true;
+            select.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            select.Title = "Select Files to Add -- Ctrl + Click to Select Multiple Files";
+            bool? result = select.ShowDialog();
+            if (result == true)
+            {
+                string[] files = select.FileNames;
+                select = null;
+                return (files);
+            }
+            else return null;
+        }
+
+        public void AddFiles(string[] files)
+        {
+            foreach (string file in files)
+            {
+                if(lstAddList.Items.Contains(file) == false) lstAddList.Items.Add(file);
+            }
+        }
+
+        public void AddPlaylist(string[] files)
+        {
+            foreach (string file in files)
+            {
+                string[] playList = File.ReadAllLines(file);
+                string filePath = System.IO.Path.GetFullPath(file);
+                string fileDir = filePath.Replace(System.IO.Path.GetFileName(file), "");
+
+                foreach (string song in playList)
+                {
+                    Regex drive = new Regex(@"([A-Za-z]):\\");
+                    string driveMatch = song.Substring(0, 3);
+                    string addFile = song;
+
+                    if (drive.IsMatch(driveMatch) == false)
+                    {
+                        addFile = System.IO.Path.Combine(fileDir, song);
+                    }
+
+                    if (File.Exists(addFile) == false)
+                    {
+                        MessageBox.Show("File " + addFile + "does not exist!", "File Not Found", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    else lstAddList.Items.Add(addFile);
+                }
+            }
+        }
+
+        private void btnMoveFolder_Click(object sender, RoutedEventArgs e)
+        {
+            WPFFolderBrowser.WPFFolderBrowserDialog folders = new WPFFolderBrowser.WPFFolderBrowserDialog();
+            folders.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            folders.Title = "Select or Create a Folder to Copy Files To";
+            
+            bool? result = folders.ShowDialog();
+            if (result == true)
+            {
+                string copyFolder = folders.FileName;
+                txtCompFolder.Text = copyFolder;
+
+                foreach (object song in lstAddList.Items)
+                {
+                    string songFileName = System.IO.Path.GetFileName(song.ToString());
+                    string newSong = System.IO.Path.Combine(copyFolder, songFileName);
+                    if (File.Exists(newSong) == false) File.Copy(song.ToString(), newSong, true);
+
+                    string newSongTitle = System.IO.Path.GetFileNameWithoutExtension(newSong);
+                    string extension = System.IO.Path.GetExtension(newSong);
+
+                    CompilationFile addFile = new CompilationFile(copyFolder, newSongTitle, extension);
+                    if(Compilation.Contains(addFile) == false) Compilation.Add(addFile);
+                }
+
+                lstAddList.Items.Clear();
+                tabEditComp.Focus();
+            }
+        }
+
+        private void lstAddList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                for (int i = lstAddList.Items.Count - 1; i >= 0; i--)
+                {
+                    if (lstAddList.SelectedItems.Contains(lstAddList.Items[i])) lstAddList.Items.Remove(lstAddList.Items[i]);
+                }
+            }
+        }
+
+        private void btnMoveUp_Click(object sender, RoutedEventArgs e)
+        {
+            int curIdx = dtaCompilation.SelectedIndex;
+            if (curIdx > 0)
+            {
+                CompilationFile item = Compilation[curIdx];
+                Compilation.RemoveAt(curIdx);
+                int newIdx = curIdx - 1;
+                Compilation.Insert(newIdx, item);
+                dtaCompilation.SelectedIndex = newIdx;
+            }
+        }
+
+        private void btnMoveDown_Click(object sender, RoutedEventArgs e)
+        {
+            int curIdx = dtaCompilation.SelectedIndex;
+            if (curIdx < Compilation.Count - 1)
+            {
+                CompilationFile item = Compilation[curIdx];
+                Compilation.RemoveAt(curIdx);
+                int newIdx = curIdx + 1;
+                Compilation.Insert(newIdx, item);
+                dtaCompilation.SelectedIndex = newIdx;
+            }
+        }
+
+        private void btnAutoNum_Click(object sender, RoutedEventArgs e)
+        {
+            for(int i = 0; i < Compilation.Count; i++)
+            {
+                string fileName;
+                Match numFound = Regex.Match(Compilation[i].FileName, @"^\d{1,}");
+                if (numFound.Success == true)
+                {
+                    fileName = Compilation[i].FileName.Remove(numFound.Index, numFound.Length);
+                }
+                else fileName = Compilation[i].FileName;
+
+                int trackNum = i + 1;
+                Compilation[i].FileName = trackNum.ToString("00") + fileName;
+            }
+        }
+
+        private void btnSelectFolder_Click(object sender, RoutedEventArgs e)
+        {
+            WPFFolderBrowser.WPFFolderBrowserDialog folders = new WPFFolderBrowser.WPFFolderBrowserDialog();
+            folders.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            folders.Title = "Select a Folder to Open";
+
+            bool? result = folders.ShowDialog();
+
+            if (result == true)
+            {
+                string openFolder = folders.FileName;
+
+                string[] files = Directory.GetFiles(openFolder, "*.mp3");
+                if (files.Length == 0)
+                {
+                    MessageBox.Show("No .mp3 files in this folder!", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+
+                txtCompFolder.Text = openFolder;
+                Compilation.Clear();
+
+                foreach (string file in files)
+                {
+                    string songTitle = System.IO.Path.GetFileNameWithoutExtension(file);
+                    string extension = System.IO.Path.GetExtension(file);
+
+                    CompilationFile addFile = new CompilationFile(openFolder, songTitle, extension);
+                    if (Compilation.Contains(addFile) == false) Compilation.Add(addFile);
+                }
+
+            }
+        }
+
+        private void btnCreatePlaylist_Click(object sender, RoutedEventArgs e)
+        {
+            string playList = string.Empty;
+
+            foreach (object song in Compilation)
+            {
+                string file = song.ToString();
+                playList += file + "\r\n";
+            }
+
+            SaveFileDialog savePlaylist = new SaveFileDialog();
+            savePlaylist.DefaultExt = "*.m3u";
+            savePlaylist.Filter = "M3U Files|*.m3u";
+            bool? save = savePlaylist.ShowDialog();
+
+            if(save == true)
+            {
+                try
+                {
+                    File.WriteAllText(savePlaylist.FileName, playList);
+                    MessageBox.Show("Playlist saved to " + savePlaylist.FileName, "File Saved", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Could not save playlist file." + "\n" + "Error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+
+        }
+
+
+    }
+
+    
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-mp3-mixtape-maker
-=================
+#MP3 Mixtape Maker
+A Windows utility for compiling, renaming and packaging files for mp3 compilations.
+
+So you wanna make a mixtape? You've got mp3 files scattered across multiple directories. Maybe you've got a playlist file containing all of the songs in the correct order? 
+
+**MP3 Mixtape Maker** takes your .m3u playlist file (supported by nearly every mp3 player software in existence) and copies all of the files to a folder of your choice. You can then rename the files, number then sequentially, and zip them.
+
+##How To Use
+From the *Add Files* tab, click the *Add MP3 Files* button to select files to add to your compilation. If you have an .m3u playlist that you wish to import, clock the *Add Playlist* button. The files you've selected will appear in the list below.
+
+![Add Files tab]()
+
+When you are ready to gather your files, click the *Copy Files to Folder* button. Select or create the folder that you wish to copy your music files to. The display will switch to the *Edit Compilation* tab.
+
+![Edit Compilation tab]()
+
+Here, you can rename and reorder files. To rename a file, simply double-click the file name in the list and start typing. Use the *Move Up* and *Move Down* buttons to re-order titles in the list. The *Auto Number* button will strip any numbering from the file names and re-number them in the order that they appear in the list.
+
+If you need to work with a previous compilation, click the *Select Folder* button (small folder icon at the far right) to select a folder to open in the *Edit Compilation* tab. 
+
+Once your files are re-named and organized, click the *Create Playlist* button to create an M3U playlist, or the *Zip Files* button to zip up the files in the list. 
+
+##To Do (maybe)
+Add mp3 tagging capabilities. There are a lot of programs however that do this.
+
+##Download
+
+**Windows Installer:** [Mp3 Mixtape Maker 0.2 beta]()
+
